8c93e24e35d52f739282ef615854d060d4b44579
casemiro <jv.casemiro@hotmail.com.br>
2020-10-09 19:38:12 -0300

    Criação de arquivo para recebimento dos dados

---
+++ action.php
@@ -0,0 +1,14 @@
+<?php
+    //Variaveis vindas do formulário
+    $nome=$_POST["nomeSobrenome"];
+    $cpf=$_POST["cpf"];
+    $telefone=$_POST["telefone"];
+    $tipodesangue=$_POST["tipoSangue"];
+    $uf=$_POST["uf"];
+    $cidade=$_POST["cidade"];
+    $bairro=$_POST["bairro"];
+    $endereco=$_POST["endereco"];
+
+    //Conexão e envio para o banco de dados
+
+?>
